d2d9ee093071b0b243208ce3b3f191bf9cb235d9
nedislavd <nedislav.denev@gmail.com>
2022-10-31 09:58:54 +0200

    Allow players to choose their piece (X or O). Done in 15 mins.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,15 @@ import { React, useEffect, useState } from "react";
 import { Board } from "../Board";
 import { calculateWinner } from "../../utils/engine.js";
 
+const textCenter = {
+    textAlign: "center",
+}
+
+const textLegend = {
+    fontSize: "0.8rem",
+    fontStyle: 'italic',
+}
+
 const gameStyle = {
   width: 250,
   margin: "20px auto",
@@ -10,11 +19,15 @@ const gameStyle = {
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXisNext] = useState(true);
+  const [playerOnePiece, setPlayerOnePiece] = useState("X");
+  const [playerTwoPiece, setPlayerTwoPiece] = useState("O");
+  const [gameStarted, setGameStarted] = useState(false);
   const winner = calculateWinner(board);
   const [draw, setDraw] = useState(false);
 
   const handleGameLogic = (i) => {
     const boardCopy = [...board];
+    setGameStarted(true);
     if (winner || boardCopy[i]) {
       return;
     }
@@ -25,15 +38,16 @@ const Game = () => {
   };
 
   useEffect(() => {
-    if (board.every((element) => element !== null)) {
+    if (!winner && board.every((element) => element !== null)) {
       setDraw(true);
     }
-  }, [board]);
+  }, [board, winner]);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setDraw(false);
     setXisNext(true);
+    setGameStarted(false);
   };
 
   const renderMoves = () => {
@@ -44,15 +58,32 @@ const Game = () => {
     );
   };
 
+  const swapPlayerPieces = () => {
+    if (playerOnePiece === "X") {
+        setPlayerOnePiece("O");
+        setPlayerTwoPiece("X");
+    } else {
+        setPlayerOnePiece("X");
+        setPlayerTwoPiece("O");
+    }
+  }
+
   return (
     <>
+      <h2 style={textCenter}>Play Tic-Tac-Toe</h2>
+      <section style={textCenter}>
+        <h4>Choose Your Piece</h4>
+        Player 1: {playerOnePiece} <button disabled={gameStarted} onClick={swapPlayerPieces}>swap</button> Player 2: {playerTwoPiece}
+          <p style={textLegend}>"X" always goes first</p>
+      </section>
       <Board squares={board} onClick={handleGameLogic} />
       <div style={gameStyle}>
         <p style={{ textAlign: "center" }}>
           {winner ? `Winner: ` + winner : ""}
           {draw
             ? `Game ended in Draw`
-            : `Next Player: ` + (xIsNext ? "X" : "O")}
+            : ""}
+          {(!draw && !winner) ?  `Next Player: ` + (xIsNext ? "X" : "O") : ""}
         </p>
         {renderMoves()}
       </div>
